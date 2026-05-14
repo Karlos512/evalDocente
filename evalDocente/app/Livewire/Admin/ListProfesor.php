8628074ff64825bee0eaafa31307a7c362b526d0
Enrique <enrique98isc@gmail.com>
2026-05-14 15:31:43 -0600

    se agrega metodo suspende profesor

--- evalDocente/app/Livewire/Admin/ListProfesor.php
+++ evalDocente/app/Livewire/Admin/ListProfesor.php
@@ -20,10 +20,20 @@ class ListProfesor extends Component
         session()->flash('message', 'Profesor eliminado correctamente');
     }
 
+    public function suspender($id)
+    {
+        $profesor = ProfesoresModel::findOrFail($id);
+        $profesor->update(['is_active' => 0 ]);
+
+        session()->flash('message', 'Profesor Suspendido');
+
+        return redirect()->route('lista-profesores');
+    }
+
     public function render()
     {
         return view('livewire.admin.list-profesor', [
-            'profesores' => ProfesoresModel::paginate(10),
+            'profesores' => ProfesoresModel::where('is_active', '1')->paginate(10),
         ]);
     }
 }
